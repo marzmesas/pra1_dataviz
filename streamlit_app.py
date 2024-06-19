f1feacf8123ed33dc295222cae5f97b619f858e6
Mario Mesas <mmesasro@gmail.com>
2024-06-19 21:06:13 +0200

    Minor refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ from streamlit_option_menu import option_menu
 from utils import open_page
 
 
-# Define the main function for Streamlit app
 def main():
     
     st.set_page_config(page_title='Data Visualization App', layout='wide', initial_sidebar_state='collapsed')
@@ -15,25 +14,23 @@ def main():
 
     with st.sidebar:
         selection = option_menu(
-            menu_title="App Menu",  # required
-            options=["Home", "Graphs"],  # required
-            icons=["house", "bar-chart"],  # optional
-            default_index=0,  # optional
+            menu_title="App Menu",  
+            options=["Home", "Graphs"],  
+            icons=["house", "bar-chart"],  
+            default_index=0, 
         )
     alt.themes.enable("dark")
 
     if selection == 'Graphs':
         df= pd.read_csv('dubai_properties.csv')
 
-        #Drop some useless columns identified in EDA
         columns_to_drop = ['Address','Frequency','Purpose','Latitude','Longitude']
         df = df.drop(columns_to_drop,axis=1)
 
-        #EDA operations
         df['Posted_date'] = df['Posted_date'].astype('datetime64[ns]')
         df['year'] = df['Posted_date'].dt.year
 
-        # Title and selection bar
+
         st.title('Selecciona tu punto de interés')
         st.markdown(
             """
@@ -53,7 +50,6 @@ def main():
                                                     'Tipos de propiedades a lo largo de los años', 
                                                     'Comparativa entre ciudades'])
 
-        # Depending on the selection, call the corresponding function
         if graph_type == 'Ubicaciones más caras en relación a su superficie':
             n_loc = st.number_input('Introduzca un número de ubicaciones para la comparativa', min_value=1, max_value=15, value=5, step=1)
             visualizations.top_n_locations(df, n_loc)
@@ -84,7 +80,6 @@ def main():
                     al usuario una pincelada informativa acerca de los datos extraídos del dataset aceca del mercado inmobiliario en UAE.\
                     Asimismo se busca entregar una experiencia de usuario sencilla y directa a través de menús y diversos tipos de gráficos interactivos. \
                     </h3>", unsafe_allow_html=True)
-        # Add background image
         st.markdown(
             """
             <style>
--- visualizations.py
+++ visualizations.py
@@ -6,15 +6,13 @@ import streamlit as st
 
 def rental_properties_pie_chart(df, year):
     apt_yearly = df.groupby(['year','Type'])['Type'].count().to_frame().rename(columns={'Type':'count'}).reset_index()
-    # Filter data for the given year
+
     yearly = apt_yearly[apt_yearly['year'] == year]
 
-    # Check if data is available for the given year
     if yearly.empty:
         st.write(f"No data available for the year {year}")
         return
 
-    # Create the Plotly pie chart
     fig = go.Figure(data=[go.Pie(
         values=yearly['count'],
         labels=yearly['Type'],
@@ -22,17 +20,16 @@ def rental_properties_pie_chart(df, year):
         hole=0.3,
     )])
 
-    # Customize the layout
     fig.update_layout(
         height=500,
-        plot_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
         font=dict(
-            family='Arial, sans-serif',  # Modern font family
+            family='Arial, sans-serif',  
             size=14,
-            color='#ffffff'  # Dark grey text color
+            color='#ffffff'
         ),
-        margin=dict(l=60, r=10, t=10, b=0),  # Adjust margins
+        margin=dict(l=60, r=10, t=10, b=0),
         annotations=[dict(
             text=f'{year}',
             x=0.5,
@@ -42,29 +39,27 @@ def rental_properties_pie_chart(df, year):
         )]
     )
 
-    # Show the plot using Streamlit
     st.plotly_chart(fig, use_container_width=True)
 
 def room_vs_rent(df):
-    # Calculate average rent per room type
+
     room_type_rent = df.groupby('Type')['Rent'].mean().sort_values(ascending=True).reset_index()
 
-    # Create the Plotly horizontal bar chart
     fig = px.bar(room_type_rent, y='Type', x='Rent', 
-                 orientation='h',  # Horizontal orientation
+                 orientation='h',
                  title='Tipo de Alojamiento vs Precio del Alquiler',
                  labels={'Type': 'Tipo de Alojamiento', 'Rent': 'Precio medio del Alquiler'},
                  color='Rent',
-                 color_continuous_scale='rainbow')  # More modern color scale
+                 color_continuous_scale='rainbow')
 
-    # Customize the layout
+
     fig.update_layout(
-        plot_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        plot_bgcolor='rgba(0,0,0,0)',  
         paper_bgcolor='rgba(0,0,0,0)',
         font=dict(
-            family='Arial, sans-serif',  # Modern font family
+            family='Arial, sans-serif',
             size=15,
-            color='#ffffff'  # Dark grey text color
+            color='#ffffff' 
         ),
         title={
             'y':0.9,
@@ -80,38 +75,33 @@ def room_vs_rent(df):
         )
     )
 
-    # Add annotations for the bars
     for idx, row in room_type_rent.iterrows():
         fig.add_annotation(
-            x=row['Rent'],  # Position based on Rent value
+            x=row['Rent'],
             y=row['Type'],
             text=f"{row['Rent']:,.2f}",
             showarrow=False,
             font=dict(size=12, color='white')
         )
 
-    # Show the plot
     st.plotly_chart(fig, use_container_width=True)
 
 def top_n_locations(df, n_loc):
-    # Calculate average rent per location
     locations_rent = df.groupby('Location')['Rent_per_sqft'].mean().sort_values(ascending=False).head(n_loc).reset_index()
 
-    # Create the Plotly barplot
     fig = px.bar(locations_rent, x='Location', y='Rent_per_sqft', 
                 title=f'Top {n_loc} ubicaciones más caras en relación a superficie alquilada',
                 labels={'Location': 'Ubicación', 'Rent_per_sqft': 'Alquiler por sqft'},
                 color='Rent_per_sqft',
-                color_continuous_scale='tealrose')  # Updated color scale
+                color_continuous_scale='tealrose')
 
-    # Customize the layout
     fig.update_layout(
-        plot_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        plot_bgcolor='rgba(0,0,0,0)',  
         paper_bgcolor='rgba(0,0,0,0)',
         font=dict(
-            family='Arial, sans-serif',  # Modern font family
+            family='Arial, sans-serif',
             size=14,
-            color='#ffffff'  # Dark grey text color
+            color='#ffffff'
         ),
         title={
             'y': 0.95,
@@ -119,14 +109,13 @@ def top_n_locations(df, n_loc):
             'xanchor': 'center',
             'yanchor': 'top'
         },
-        height=600,  # Adjusting the height
-        margin=dict(l=50, r=50, t=100, b=50),  # Add margins for better spacing
+        height=600,
+        margin=dict(l=50, r=50, t=100, b=50),
         yaxis = dict(showgrid=False)
     )
 
-    # Add annotations for the bars
     for idx, row in locations_rent.iterrows():
-        y_offset = row['Rent_per_sqft'] * 0.1  # Adjust the factor for spacing
+        y_offset = row['Rent_per_sqft'] * 0.1
         fig.add_annotation(
             x=row['Location'],
             y=row['Rent_per_sqft'] + y_offset,
@@ -135,31 +124,29 @@ def top_n_locations(df, n_loc):
             font=dict(size=12, color='white')
         )
 
-    # Rotate x-axis labels for better readability
+
     fig.update_xaxes(tickangle=45)
 
-    # Show the plot using st.plotly_chart centered
+
     st.plotly_chart(fig, use_container_width=True)
 
 def city_area_rent_graph(df):
-    # Calculate average area and rent per city
+
     city_area_rent = df.groupby('City').agg({'Area_in_sqft': 'mean', 'Rent': 'mean'}).reset_index()
 
-    # Create the Plotly barplot for Rent
     fig2 = px.bar(city_area_rent, x='City', y='Rent', 
                   title='Alquiler medio por ciudad',
                   labels={'City': 'Ciudad', 'Rent': 'Alquiler medio'},
                   color='Rent',
                   color_continuous_scale='algae')
 
-    # Customize the layout for the second plot
     fig2.update_layout(
-        plot_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
         font=dict(
-            family='Arial, sans-serif',  # Modern font family
+            family='Arial, sans-serif',
             size=14,
-            color='#ffffff'  # Dark grey text color
+            color='#ffffff'
         ),
         title={
             'y': 0.9,
@@ -170,7 +157,7 @@ def city_area_rent_graph(df):
         yaxis=dict(range=[0, 250000], showgrid=False)
     )
     for idx, row in city_area_rent.iterrows():
-        y_offset = row['Rent'] * 0.1  # Adjust the factor for spacing
+        y_offset = row['Rent'] * 0.1 
         fig2.add_annotation(
             x=row['City'],
             y=row['Rent'] + y_offset,
